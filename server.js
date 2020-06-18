0ca2191c900fc3e28303796792077fc1d833b211
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-06-18 11:34:25 -0400

    Enabled saving notes to db.json

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,10 +11,7 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //initialize note list and note ID
-let notes = []
-let noteCount = 0;
-
-// app.get('/')
+let notes = JSON.parse( fs.readFileSync( './db.json', 'utf8') )
 
 //GET endpoint
 app.get("/api/notes", function( req, res){
@@ -23,9 +21,9 @@ app.get("/api/notes", function( req, res){
 //POST endpoint
 app.post("/api/notes/", function( req, res){
     const newNote = req.body
-    newNote.id = noteCount;
+    newNote.id = Date.now();
     notes.push( newNote );
-    noteCount++;
+    fs.writeFileSync( './db.json', JSON.stringify(notes) )
     res.send( notes )
 } )
 
@@ -33,9 +31,21 @@ app.post("/api/notes/", function( req, res){
 app.delete("/api/notes/:id", function( req, res){
     const noteID = req.params.id
     notes = notes.filter( note => note.id != noteID )
-    console.log( notes )
-
+    // console.log( notes )
+    fs.writeFileSync( './db.json', JSON.stringify(notes) )
     res.send( notes )
+})
+
+app.put( '/api/notes', function( req, res ){
+    const revidesNote = req.body
+    if( !revidesNote.id ){
+        res.send( { stauts: false, message: `Id not found` }  )
+    }
+    const noteIdx = notes.findIndex( revidesNote )
+
+    notes[noteIdx] = revidesNote
+    fs.writeFileSync( './db.json', JSON.stringify(notes) )
+    res.send( { status: true, message: `Note revised` } )
 })
 
 //PORT Listener
